0be9d738c3ef9fff26876ddad55723c5dc1a65ae
Balint Korosi <balint.j.korosi@gmail.com>
2023-01-12 16:01:42 +0100

    headline copy change

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -3,12 +3,12 @@ import React from "react";
 const Rank = () => {
   return(
     <div >
-      <div className="white f3">
+      {/* <div className="white f3">
         {'Bálint, Your current rank is...'}
       </div>
       <div className="white f1">
         {'#5'}
-      </div>
+      </div> */}
     </div>
     
   )
