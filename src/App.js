670b38c9fac6427a6691916ee0c1c384564946a3
Balint Korosi <balint.j.korosi@gmail.com>
2023-01-13 07:29:43 +0100

    routing done

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ParticlesBg from 'particles-bg';
 import React, {Component} from 'react';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
 
 window.process = { };
 
@@ -34,6 +35,7 @@ class App extends Component {
       imageUrl: '',
       box: {},
       route: 'signin',
+      isSignedIn: false,
     }
   }
   
@@ -97,22 +99,34 @@ class App extends Component {
   }
 
   onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+    this.setState({isSignedIn: true});
+    }
     this.setState({route: route});
   }
   render() {
+   const {isSignedIn, imageUrl, route, box} = this.state;
     return (
       <div className="App">
         {/* <ParticlesBg type="cobweb" bg={true} /> */}
         <ParticlesBg {...PARTICLES_BG_PROPS} />
-        <Navigation onRouteChange={this.onRouteChange} />
-        { this.state.route === 'signin' 
-        ? <Signin onRouteChange={this.onRouteChange} />
-        : <div>
-            <Logo />
-            <Rank />
-            <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}  />
-            <FaceRecognition box={this.state.box}  imageUrl={this.state.imageUrl}/>
-          </div>
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        { route === 'home' 
+        ? <div>
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}  />
+        <FaceRecognition box={box}  imageUrl={imageUrl}/>
+      </div> 
+        : (
+          route === 'signin' 
+          ? <Signin onRouteChange={this.onRouteChange} />
+          : <Register onRouteChange={this.onRouteChange} />
+        )
+        
+         
         }
       </div>
     );
