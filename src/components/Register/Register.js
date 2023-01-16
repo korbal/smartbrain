8f1fb2700a81c7cdcee43ce790d0f5e3613aee1f
Balint Korosi <balint.j.korosi@gmail.com>
2023-01-16 15:14:45 +0100

    final commit before adding clarify to backend

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -29,11 +29,11 @@ class Register extends React.Component {
         email: this.state.email,
         password: this.state.password, 
         name: this.state.name
-      })
+      })   
     })
     .then(response => response.json())
     .then(user => {
-      if(user) {
+      if(user.id) {
         this.props.loadUser(user);
         this.props.onRouteChange('home');
       }
