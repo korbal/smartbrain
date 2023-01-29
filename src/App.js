ecfda80dd3f785bb3d869b356e1e2c1ff091e27d
Balint Korosi <balint.j.korosi@gmail.com>
2023-01-29 10:26:52 +0100

    added aws lambda function to backend to calculate rank

--- src/App.js
+++ src/App.js
@@ -1,50 +1,49 @@
-import './App.css';
-import Navigation from './components/Navigation/Navigation';
-import Logo from './components/Logo/Logo';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import Rank from './components/Rank/Rank';
-import ParticlesBg from 'particles-bg';
-import React, {Component} from 'react';
-import FaceRecognition from './components/FaceRecognition/FaceRecognition';
-import Signin from './components/Signin/Signin';
-import Register from './components/Register/Register';
-import Modal from './components/Modal/Modal';
-import Profile from './components/Profile/Profile';
+import "./App.css";
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import ParticlesBg from "particles-bg";
+import React, { Component } from "react";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+import Signin from "./components/Signin/Signin";
+import Register from "./components/Register/Register";
+import Modal from "./components/Modal/Modal";
+import Profile from "./components/Profile/Profile";
 
 //backend url
-const BACKEND_BASE_URL = "https://smartbrain.cyclic.app";
+//const BACKEND_BASE_URL = "https://smartbrain.cyclic.app";
 
 // development backend url
-//const BACKEND_BASE_URL = "http://localhost:3000";
+const BACKEND_BASE_URL = "http://localhost:3000";
 const PARTICLES_BG_PROPS = {
   type: "cobweb",
   bg: true,
   num: 200,
-  color:"#ffffff",
-}
+  color: "#ffffff",
+};
 
-window.process = { };
+window.process = {};
 
 /////////////////////////// MAIN APP /////////////////////////////////////
 
 const initialState = {
-  input: '',
-  imageUrl: '',
+  input: "",
+  imageUrl: "",
   box: [],
-  route: 'signin',
+  route: "signin",
   isSignedIn: false,
   isProfileOpen: false,
   user: {
-    id: '',
-    name: '',
-    email: '',
+    id: "",
+    name: "",
+    email: "",
     entries: 0,
-    joined: '', 
-    pet: '',
-    age: ''
-  }
-}
-
+    joined: "",
+    pet: "",
+    age: "",
+  },
+};
 
 class App extends Component {
   constructor() {
@@ -52,121 +51,137 @@ class App extends Component {
     this.state = initialState;
   }
 
- 
   loadUser = (data) => {
-    this.setState({user: {
-      id: data.id,
-      name: data.name,
-      email: data.email,
-      entries: data.entries,
-      joined: data.joined
-    }})
-  }
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined,
+      },
+    });
+  };
 
   calculateFaceLocation = (boxdataArray) => {
     //console.log(boxdataArray)
-    const image = document.getElementById('inputimage');
+    const image = document.getElementById("inputimage");
     const width = Number(image.width);
     const height = Number(image.height);
-    return boxdataArray.map(boxdata => {
-        return {
-            leftCol: boxdata.left_col * width,
-            topRow: boxdata.top_row * height,
-            rightCol: width - (boxdata.right_col * width),
-            bottomRow: height - (boxdata.bottom_row * height)
-        }
+    return boxdataArray.map((boxdata) => {
+      return {
+        leftCol: boxdata.left_col * width,
+        topRow: boxdata.top_row * height,
+        rightCol: width - boxdata.right_col * width,
+        bottomRow: height - boxdata.bottom_row * height,
+      };
     });
-}
+  };
 
   displayFaceBox = (box) => {
     //console.log(box);
-    this.setState({box: box});
-
-
-  }
+    this.setState({ box: box });
+  };
 
   onInputChange = (event) => {
-    this.setState({input: event.target.value});
-  }
-  
+    this.setState({ input: event.target.value });
+  };
+
   onButtonSubmit = () => {
-    this.setState({imageUrl: this.state.input}); // set imageUrl to input value
+    this.setState({ imageUrl: this.state.input }); // set imageUrl to input value
     // request to backend to handle clarifai api call
-        fetch(`${BACKEND_BASE_URL}/imageurl`, {
-      method: 'post',
-      headers: {'Content-Type': 'application/json'},
+    fetch(`${BACKEND_BASE_URL}/imageurl`, {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        input: this.state.input
+        input: this.state.input,
+      }),
+    })
+      .then((response) => response.json()) // if response is ok, gives back boxdata to frame the face of the picture
+      // check the response here, if it's an array of bounding boxes
+      .then((boxdata) => {
+        if (boxdata) {
+          fetch(`${BACKEND_BASE_URL}/image`, {
+            method: "put",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+              id: this.state.user.id,
+            }),
+          })
+            .then((response) => response.json())
+            .then((count) => {
+              this.setState(Object.assign(this.state.user, { entries: count })); // store proper user data in state
+            })
+            .catch(console.log);
+        }
+
+        this.displayFaceBox(this.calculateFaceLocation(boxdata));
       })
-    })
-    .then(response => response.json()) // if response is ok, gives back boxdata to frame the face of the picture
-     // check the response here, if it's an array of bounding boxes
-    .then(boxdata => {
-      if(boxdata) {
-        fetch(`${BACKEND_BASE_URL}/image`, {
-          method: 'put',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify({
-            id: this.state.user.id
-          })
-          })
-          .then(response => response.json())
-          .then(count => {this.setState(Object.assign(this.state.user, {entries: count})) // store proper user data in state
-          })
-          .catch(console.log)
-       }
-      
-       this.displayFaceBox(this.calculateFaceLocation(boxdata))
-    })
-    .catch(err => console.log(err))
-  }
+      .catch((err) => console.log(err));
+  };
 
   onRouteChange = (route) => {
-    if (route === 'signout') {
-     return this.setState(initialState)
-    } else if (route === 'home') {
-    this.setState({isSignedIn: true});
+    if (route === "signout") {
+      return this.setState(initialState);
+    } else if (route === "home") {
+      this.setState({ isSignedIn: true });
     }
-    this.setState({route: route});
-  }
+    this.setState({ route: route });
+  };
 
   toggleModal = () => {
-    this.setState(prevState => ({
+    this.setState((prevState) => ({
       ...prevState,
-      isProfileOpen: !prevState.isProfileOpen
+      isProfileOpen: !prevState.isProfileOpen,
     }));
-  }
+  };
 
   render() {
-   const {isSignedIn, imageUrl, route, box, isProfileOpen, user} = this.state;
+    const { isSignedIn, imageUrl, route, box, isProfileOpen, user } =
+      this.state;
     return (
       <div className="App">
         <ParticlesBg {...PARTICLES_BG_PROPS} />
-        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} toggleModal={this.toggleModal}/>
+        <Navigation
+          isSignedIn={isSignedIn}
+          onRouteChange={this.onRouteChange}
+          toggleModal={this.toggleModal}
+        />
 
-        {isProfileOpen &&
-        <Modal>
-          <Profile isProfileOpen={isProfileOpen} toggleModal={this.toggleModal} user={user} />
-        </Modal>
-        }
-        { route === 'home' 
-        ? <div>
-        <Logo />
-        
+        {isProfileOpen && (
+          <Modal>
+            <Profile
+              isProfileOpen={isProfileOpen}
+              toggleModal={this.toggleModal}
+              user={user}
+            />
+          </Modal>
+        )}
+        {route === "home" ? (
+          <div>
+            <Logo />
 
-        <Rank name={this.state.user.name} entries={this.state.user.entries} />
-        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}  />
-        <FaceRecognition box={box}  imageUrl={imageUrl}/>
-      </div> 
-        : (
-          route === 'signin' 
-          ? <Signin loadUser={this.loadUser}  onRouteChange={this.onRouteChange} />
-          : <Register loadUser={this.loadUser}  onRouteChange={this.onRouteChange} />
-        )
-        }
+            <Rank
+              name={this.state.user.name}
+              entries={this.state.user.entries}
+            />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRecognition box={box} imageUrl={imageUrl} />
+          </div>
+        ) : route === "signin" ? (
+          <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register
+            loadUser={this.loadUser}
+            onRouteChange={this.onRouteChange}
+          />
+        )}
       </div>
     );
   }
 }
-export {BACKEND_BASE_URL};
+export { BACKEND_BASE_URL };
 export default App;
--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,17 +1,55 @@
 import React from "react";
 
-const Rank = ({name, entries}) => {
-  return(
-    <div >
-      <div className="white f3">
-        {`${name}, Your current entry count is...`}
+class Rank extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      emoji: "",
+    };
+  }
+  componentDidMount() {
+    this.generateEmoji(this.props.entries);
+  }
+
+  // need this hook, because the component is only changing the state at mount. but if the user adds a new photo, and the rank changes, it wouldn't reflect on the screen. this is why we need the update hook
+
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      prevProps.entries === this.state.entries &&
+      prevProps.name === this.props.name
+    ) {
+      return null;
+    }
+    this.generateEmoji(this.props.entries);
+  }
+
+  generateEmoji = (entries) => {
+    console.log(entries);
+    fetch(
+      `https://cr6mmat6p5.execute-api.us-east-1.amazonaws.com/rank?rank=${entries}`
+    )
+      .then((response) => response.json())
+      .then((data) => this.setState({ emoji: data.input }));
+  };
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextProps.entries === this.props.entries) {
+      return false;
+    }
+    return true;
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="white f3">
+          {`${this.props.name}, Your current entry count is...`}
+        </div>
+        <div className="white f1">{`#${this.props.entries}`}</div>
+        <div className="white f3">{`Rank Badge: ${this.state.emoji}`}</div>
       </div>
-      <div className="white f1">
-        {`#${entries}`}
-      </div>
-    </div>
-    
-  )
+    );
+  }
 }
 
 export default Rank;
